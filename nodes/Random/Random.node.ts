4a7a7a80e5e617209cae328f999ef8ab8a65c3d0
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-09-21 12:23:34 -0300

    feat: n8n description e properties

--- nodes/Random/Random.node.ts
+++ nodes/Random/Random.node.ts
@@ -7,37 +7,54 @@ import {
 
 export class Random implements INodeType {
   description: INodeTypeDescription = {
-    displayName: "",
-    name: "",
-    icon: "file:node.svg",
+    displayName: "Random",
+    name: "random",
+    icon: "file:random.svg",
     group: ["transform"],
     version: 1,
-    description: "",
+    description: "Generates random numbers",
     defaults: {
-      name: "node",
+      name: "Random",
       color: "#9391ffff",
     },
     inputs: ["main"],
     outputs: ["main"],
     properties: [
+
       {
         displayName: "Resource",
         name: "resource",
         type: "options",
         options: [
           {
-            name: "",
-            value: "",
+            name: "True Random Number Generator",
+            value: "trueRandomNumber",
           },
         ],
-        default: "",
+        default: "trueRandomNumber",
         noDataExpression: true,
       },
-    ]
+      {
+        displayName: "Min:",
+        name: "minNumber",
+        type: "number",
+        default: 1,
+        required: true,
+        description: "The minimun range value for the random generator",
+      },
+      {
+        displayName: "Max:",
+        name: "maxNumber",
+        type: "number",
+        default: 100,
+        required: true,
+        description: "The maximum range value for the random generator",
+      },
+    ],
   };
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 
-    return [this.helpers.returnJsonArray(returnData)];
+    return [this.helpers.returnJsonArray({})];
   }
 }
